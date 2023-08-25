f23befec0c30be49707054f7546398c9891aebb5
diego joao <diegosgjoao@gmail.com>
2023-08-25 09:01:11 -0300

    mvc almost ready no style

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,9 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: %i[show create new]
 
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
+    @movies = Movie.all
+    @list = List.find(params[:list_id])
   end
 
   def show; end
@@ -12,17 +12,21 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit; end
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
 
   private
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
