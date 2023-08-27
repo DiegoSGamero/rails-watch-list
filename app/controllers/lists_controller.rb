88f57a7bed874fad31928aa3d3bd524aa2b97388
diego joao <diegosgjoao@gmail.com>
2023-08-27 16:37:12 -0300

    installing cloudinary gem and some style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,8 +15,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list), notice: 'List was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit; end
@@ -39,6 +42,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
